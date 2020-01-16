270a95c3b15f8772a3b9e6bf3cf592195ac125f6
Matthew Doering <mattdoering86@gmail.com>
2020-01-15 22:53:07 -0500

    worked on specialty page added content and video

---
+++ assets/js/sbs_edits.js
@@ -0,0 +1,24 @@
+let pipeList = document.querySelector('.list')
+pipeList.innerHTML += `<ul class="specialty-list col-10-mobile off-2-mobile">
+    <li class="list-building">
+      Corrosion
+    </li>
+    <li class="list-building">
+      Leaching of lead from corroded joints
+    </li>
+    <li class="list-building">
+      Chronic Scaling
+    </li>
+    <li class="list-building">
+      Joint leaks
+    </li>
+    <li class="list-building">
+      Pin-holding
+    </li>
+    <li class="list-building">
+      Water quality deterioration
+    </li>
+    <li class="list-building">
+      Flow deteriroration
+    </li>
+  </ul>`
